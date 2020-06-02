97507bce511b34060917acaefe8763806c429ac5
matiasbian <matiasezequielbian@gmail.com>
2020-06-02 19:40:42 -0300

    Updates

---
+++ precache-manifest.e1cd1d34d34eccf7264d9aae727a9290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9deb96f33da3c48798ee48549b522c",
+    "url": "/TalleresBiblioteca/index.html"
+  },
+  {
+    "revision": "9b610521467db9769982",
+    "url": "/TalleresBiblioteca/static/css/main.2e7b5a84.chunk.css"
+  },
+  {
+    "revision": "57d38bcc6a8b76e1f630",
+    "url": "/TalleresBiblioteca/static/js/2.98b8a772.chunk.js"
+  },
+  {
+    "revision": "9b610521467db9769982",
+    "url": "/TalleresBiblioteca/static/js/main.9da52bc4.chunk.js"
+  },
+  {
+    "revision": "3e8db0e8ed52186c6297",
+    "url": "/TalleresBiblioteca/static/js/runtime~main.edc22757.js"
+  }
+]);
